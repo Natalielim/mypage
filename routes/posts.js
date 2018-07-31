d3d00b5b484f288ed66594db8b63254ab4cb5c4b
natalielim <natalielim@gmail.com>
2018-07-31 11:54:04 -0700

    installed multer and s3-uploader

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,51 @@ const auth = require('./helpers/auth');
 const User = require('../models/user');
 const Post = require('../models/post');
 
+// const multer  = require('multer');
+//
+// const storage = multer.diskStorage({
+//   filename: function (req, file, cb) {
+//       // TODO Look into edge cases
+//       let extArray = file.mimetype.split("/");
+//       let ext = extArray[extArray.length - 1];
+//       cb(null, Date.now() + "." + ext);
+//   }
+// });
+//
+// const upload = multer({ storage });
+// const Upload = require('s3-uploader');
+//
+// let client = new Upload(process.env.S3_BUCKET, {
+//   aws: {
+//     path: 'mypage/images',
+//     region: process.env.S3_REGION,
+//     acl: 'public-read',
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//   },
+//   cleanup: {
+//     versions: true,
+//     original: true
+//   },
+//   versions: [{
+//     maxWidth: 320,
+//     aspect: '1.618:1',
+//     suffix: '-thumbnail'
+//   },{
+//     maxWidth: 1000,
+//     aspect: '2.414:1', //silver ratio
+//     suffix: '-desktop'
+//   },{
+//     maxWidth: 320,
+//     aspect: '2.414:1', //silver ratio
+//     suffix: '-mobile'
+//   },{
+//     maxWidth: 100,
+//     aspect: '1:1',
+//     suffix: '-square'
+//   }]
+// });
+
 // Posts new
 router.get('/new', auth.requireLogin, (req, res, next) => {
   User.findById(req.params.userId, function(err, user) {
